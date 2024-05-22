92c9ba3ab44af784f8ddcfc973ea1e12acee840d
NooobMaster12 <isaqueabraaodesousa0000@gmail.com>
2024-05-22 20:07:44 -0300

    models

--- login/models.py
+++ login/models.py
@@ -1,8 +1,29 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class Login(models.Model):
-    matricula = models.IntegerField(max_length=100)
-    senha = models.IntegerField(max_length=100)
+class Usuario(AbstractUser):
+    e_aluno = models.BooleanField(default=False)
+    e_funcionario = models.BooleanField(default=False)
+    
+    
+class PerfilAluno(models.Model):
+    matricula = models.CharField(max_length=20, unique=True)
+    usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE)
+    email = models.EmailField(unique=True)
+    curso = models.CharField(max_length=100)
+    nome_responsavel = models.CharField(max_length=100)
+    ano_ingresso = models.PositiveIntegerField()
 
     def __str__(self):
-        return self.matricula
+        return self.usuario.username
+
+
+
+class PerfilFuncionario(models.Model):
+    matricula_funcionario = models.CharField(max_length=20, unique=True)
+    usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE)
+    departamento = models.CharField(max_length=100)
+    cargo = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.usuario.username
