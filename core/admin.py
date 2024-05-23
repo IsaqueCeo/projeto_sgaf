1b2dcee0965cc1bf0b72b9da667ba2010acaecad
olivercz1 <labelwendell@gmail.com>
2024-05-23 20:09:49 -0300

    Edição do Admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+ 
 # Register your models here.
+
+
+#from .models import Dadospessoais, Empresa, Setor, Funcionario, Aluno
+
+# Registrar todos os modelos no admin
+#admin.site.register(Dadospessoais)
+#admin.site.register(Empresa)
+#admin.site.register(Setor)
+#admin.site.register(Funcionario)
+#admin.site.register(Aluno)
