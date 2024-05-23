0974d761ce347824b9861d8e5ade63c9424eb329
willianjonhy05 <willianjohny05@gmail.com>
2024-05-23 19:07:46 -0300

    View da Lista de Setores

--- core/views.py
+++ core/views.py
@@ -1,9 +1,11 @@
 from .models import Setor
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from .forms import NovoSetorForm
 from django.contrib import messages
 from django.urls import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 # Create your views here.
 
 #View para criar um novo setor
@@ -19,3 +21,14 @@ class NovoSetor(LoginRequiredMixin, CreateView):
     def get_success_url(self):
         messages.add_message(self.request, messages.SUCCESS, "Setor criado com sucesso!")
         return reverse('home')
+
+
+@login_required
+def lista_setores(request):
+    template_name = 'empresa/setores.html'
+    empresa = request.user.funcionario.empresa
+    setores = Setor.objects.filter(empresa=empresa)       
+    context = {
+        'setores': setores,
+    }
+    return render(request, template_name, context)
