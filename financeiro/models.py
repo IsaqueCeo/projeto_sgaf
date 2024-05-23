395c0bba14bf33c99d97fffa523924c66b9231d0
Gleiser Wesley <38473705+gwpoliveira@users.noreply.github.com>
2024-05-23 14:58:07 -0300

    Model Financeiro

--- financeiro/models.py
+++ financeiro/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.utils import timezone
-from .models import login
+from Login.models import Aluno
 
-class Pagamentos(models.Model):
+class Pagamento(models.Model):
     PENDENTE = 'PENDENTE'
     PAGO = 'PAGO'
     ATRASADO = 'ATRASADO'
@@ -15,8 +15,8 @@ class Pagamentos(models.Model):
         (CANCELADO, CANCELADO),
     ]
 
-    aluno = models.ForeignKey(login, on_delete=models.CASCADE)
-    valor_toal = models.DecimalField(max_digits=10, decimal_places=2)
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    valor_total = models.DecimalField(max_digits=10, decimal_places=2)
     data_criacao = models.DateField(default=timezone.now)
     data_vencimento = models.DateField()
     data_pagamento = models.DateField(null=True, blank=True)
@@ -25,7 +25,79 @@ class Pagamentos(models.Model):
         choices = STATUS_CHOICES,
         default = PENDENTE,
     )
+    def __str__(self):
+        return f'Pagamento {self.id} - Aluno: {self.aluno.nome}'
+        # Verificar o nome do aluno se está criado
+
+class Inadimplencia(models.Model):
+    pagamento = models.ForeignKey(Pagamento, on_delete=models.CASCADE)
+    data_vencimento = models.DateField()
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'Inadimplencia {self.id} - Pagamento: {self.pagamento.id}'
+
+class HistoricoPagamento(models.Model):
+    pagamento = models.ForeignKey(Pagamento, on_delete=models.CASCADE)
+    valor_pago = models.DecimalField(max_digits=10, decimal_places=2)
+    data_pagamento = models.DateField()
+    metodo_pagamento = models.CharField(max_length=50)
+    status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'Histórico Pagamento {self.id} - Pagamento: {self.pagamento.id}'
+
+class Fatura(models.Model):
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    valor_toral = models.DecimalField(max_digits=10, decimal_places=2)
+    data_emissao = models.DateField(default=timezone.now)
+    data_vencimento = models.DateField()
+    status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'Fatura {self.id} - Aluno: {self.aluno.nome}'
+
+class Desconto(models.Model):
+    fatura = models.ForeignKey(Fatura, on_delete=models.CASCADE)
+    tipo = models.CharField(max_length=50)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    data_concessao = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return f'Desconto {self.id} - Fatura {self.fatura.id}'
+
+class Taxa(models.Model):
+    nome = models.CharField(max_length=50)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    frequencia = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'Taxa {self.id} - {self.nome}'
+
+class PagamentoTaxa(models.Model):
+    pagamento = models.ForeignKey(Pagamento, on_delete=models.CASCADE)
+    taxa = models.ForeignKey(Taxa, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Pagamento {self.pagamento.id} - Taxa {self.taxa.nome}'
+
+class Multa(models.Model):
+    pagamento = models.ForeignKey(Pagamento, on_delete=models.CASCADE)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    data_aplicacao = models.DateField(default=timezone.now)
+    status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'Multa {self.id} - PAgamento {self.pagamento.id}'
+
+class BolsaEstudo(models.Model):
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    tipo = models.CharField(max_length=50)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    data_concessao = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return f'Bolsa {self.id} - Aluno {self.aluno.nome}'
 
 
-
-# Create your models here.
