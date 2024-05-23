04a590c232695095809f07dd5151d748b512c145
William Sousa <146144449+willianjonhy05@users.noreply.github.com>
2024-05-23 17:04:42 -0300

    Novos signals

--- core/signals.py
+++ core/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from .models import Empresa
+from .models import Empresa, Dadospessoais
 import requests
 
 @receiver(pre_save, sender=Empresa)
@@ -31,3 +31,40 @@ def preencher_dados_pelo_cnpj(sender, instance, **kwargs):
                 instance.natureza_juridica = dados.get("natureza_juridica", "")
         except requests.exceptions.RequestException as e:
             print(f"Erro ao obter dados do CNPJ: {e}")
+
+
+@receiver(post_save, sender=Funcionario)
+def criar_dados_pessoais_do_funcionario(sender, instance, created, **kwargs):
+    if created:
+        dados_funcionario = Dadospessoais.objects.create(
+            nome=instance.nome,
+            data_nascimento=instance.data_nascimento,
+            uf_naturalizado=instance.uf_naturalizado,
+            uf=instance.uf,
+            cep=instance.cep,
+            cidade=instance.cidade,
+            endereco=instance.endereco,
+            bairro=instance.bairro,
+            numero=instance.numero,
+            nacionalidade=instance.nacionalidade,
+            cpf=instance.cpf,
+            rg=instance.rg
+        )
+        dados_funcionario.save()
+        instance.dados_pessoais = dados_funcionario
+        instance.save()
+
+
+# @receiver(post_save, sender=Funcionario)
+# def criar_funcionario(sender, instance, created, **kwargs):
+#     if created:
+#         funcionario = User.objects.create(
+#             username=instance.matricula,              
+#             email=instance.email, 
+#             first_name=instance.nome.split()[0],
+#             last_name=' '.join(instance.nome.split()[1:])
+#             )
+#         funcionario.set_password(instance.cpf)
+#         funcionario.save()
+#         instance.usuario = funcionario
+#         instance.save()
