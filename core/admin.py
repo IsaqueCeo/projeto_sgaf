3d9fdd503cdc5ec13e15e6a69522bcdc92d1f189
William Sousa <146144449+willianjonhy05@users.noreply.github.com>
2024-05-23 23:41:13 -0300

    Edição dos admin do core

--- core/admin.py
+++ core/admin.py
@@ -1,13 +1,35 @@
 from django.contrib import admin
+from .models import Empresa, Setor, Funcionario, Aluno, Dadospessoais
  
 # Register your models here.
 
+@admin.register(Empresa)
+class EmpresaAdmin(admin.ModelAdmin):
+    list_display = ["id", "nome_fantasia", "cnpj", "email", "telefone", "cidade", "confirmado"]
+    search_fields = ["nome_fantasia", "cnpj"]
+    list_per_page = 10
+    list_editable = ["confirmado"]
 
-#from .models import Dadospessoais, Empresa, Setor, Funcionario, Aluno
+@admin.register(Setor)
+class SetorAdmin(admin.ModelAdmin):
+    list_display = ["id", "empresa", "nome", "telefone"]
+    search_fields = ["nome"]
+    list_per_page = 10
 
-# Registrar todos os modelos no admin
-#admin.site.register(Dadospessoais)
-#admin.site.register(Empresa)
-#admin.site.register(Setor)
-#admin.site.register(Funcionario)
-#admin.site.register(Aluno)
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ["id", "empresa", "matricula", "nome", "cpf", "telefone", "email"]
+    search_fields = ["nome", "matricula", "cpf"]
+    list_per_page = 10
+
+@admin.register(Aluno)
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ["id", "empresa", "matricula", "nome", "cpf", "telefone", "email"]
+    search_fields = ["nome", "matricula", "cpf"]
+    list_per_page = 10
+
+@admin.register(Dadospessoais)
+class DadospessoaisAdmin(admin.ModelAdmin):
+    list_display = ["id", "nome", "cpf", "rg"]
+    search_fields = ["nome", "cpf"]
+    list_per_page = 10
