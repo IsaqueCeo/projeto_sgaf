978bda809a42149b332a13aec029a890b9688f77
William Sousa <146144449+willianjonhy05@users.noreply.github.com>
2024-05-23 23:27:23 -0300

    Class Users

--- login/models.py
+++ login/models.py
@@ -1,4 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
 
+
+class User(AbstractUser):
+    username = models.CharField(max_length=100, help_text=(
+        "Seu nome de usuário corresponde à matrícula."
+    ))
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.username
