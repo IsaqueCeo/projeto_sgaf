12618866a5782baafa59f9f9b77a735b77fd16bb
willianjonhy05 <willianjohny05@gmail.com>
2024-06-03 18:34:48 -0300

    Novas views para professor

--- portal_do_professor/urls.py
+++ portal_do_professor/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import perfil_professor
+from .views import perfil_professor, atualizar_dados_pessoais_professor, listar_disciplinas_do_professor
 
 urlpatterns = [
     path('', perfil_professor, name='perfil-professor'),
+    path('atualizar-dados-pessoais/<int:id>/', atualizar_dados_pessoais_professor, name='atualizar-dados-pessoais-professor'),
+    path('minhas-disciplinas/<int:id>', listar_disciplinas_do_professor, name='listar_disciplina_por_professor'),
 
 ]
