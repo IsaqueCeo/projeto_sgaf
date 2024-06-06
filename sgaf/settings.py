0ab8f0a127948a0be3bfdf783bcb4c9b5077e532
Gleiser Wesley <38473705+gwpoliveira@users.noreply.github.com>
2024-06-05 21:28:03 -0300

    Banco de Dados e Senttin

--- sgaf/settings.py
+++ sgaf/settings.py
@@ -88,9 +88,13 @@ WSGI_APPLICATION = 'sgaf.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "academydb",
+        "USER": "user_academy",
+        "PASSWORD": "academy10x",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
